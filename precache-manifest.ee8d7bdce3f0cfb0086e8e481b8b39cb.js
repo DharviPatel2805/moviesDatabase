38f9008454b6227d2ffe80c9462e665561e64dc5
Dharvi Patel <83693139+DharviPatel2805@users.noreply.github.com>
2021-07-24 10:45:16 +0530

    Updates

---
+++ precache-manifest.ee8d7bdce3f0cfb0086e8e481b8b39cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c547f568aee924647c2a4e2fc94606",
+    "url": "/moviesDatabase/index.html"
+  },
+  {
+    "revision": "3da973691a0472601268",
+    "url": "/moviesDatabase/static/css/main.4e1991a4.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/moviesDatabase/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/moviesDatabase/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da973691a0472601268",
+    "url": "/moviesDatabase/static/js/main.74e9d9fe.chunk.js"
+  },
+  {
+    "revision": "eddce00fc6a0644db989",
+    "url": "/moviesDatabase/static/js/runtime-main.8bc07d25.js"
+  }
+]);
